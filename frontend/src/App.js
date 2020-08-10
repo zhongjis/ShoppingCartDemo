6fe72700bb34d0c0e1aeddbcb990cee465999fea
Zhongjie Shen <zhongjis@uci.edu>
2020-08-10 00:53:44 -0700

    [feature] move product list to HomeScreen.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,30 +44,8 @@ function App() {
         </aside>
         <main className="main">
           <div className="content">
-            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/product/:id" exact={true} component={ProductScreen} />
             <Route path="/" exact={true} component={HomeScreen} />
-
-            <ul className="products">
-            {
-              data.products.map(product => 
-                <li>
-                  <div className="product">
-                    <img
-                      className="product-image"
-                      src={product.image}
-                      alt="product"
-                    />
-                    <div className="product-name">
-                      <a href="product.html">{product.name}</a>
-                    </div>
-                    <div className="product-brand">{product.brand}</div>
-                    <div className="product-price">${product.price}</div>
-                    <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
-                  </div>
-                </li>
-              )
-            }
-            </ul>
           </div>
         </main>
         <footer className="footer">All right reserved.</footer>
