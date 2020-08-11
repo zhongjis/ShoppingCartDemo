276841c8edefaa996924cfb37c688e3621f46f73
Zhongjie Shen <zhongjis@uci.edu>
2020-08-10 23:41:25 -0700

    [backend] update homeScreen using data from express backend

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,32 +1,49 @@
-import React from "react";
-import {Link} from "react-router-dom";
-
-import data from "../data";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
 function HomeScreen(props) {
-    return <ul className="products">
-            {
-              data.products.map(product => 
-                <li>
-                  <div className="product">
-                    <Link to={'/product/' + product._id}>
-                        <img
-                          className="product-image"
-                          src={product.image}
-                          alt="product"
-                        />
-                    </Link>
-                    <div className="product-name">
-                      <Link to={'/product/' + product._id}>{product.name}</Link>
-                    </div>
-                    <div className="product-brand">{product.brand}</div>
-                    <div className="product-price">${product.price}</div>
-                    <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
-                  </div>
-                </li>
-              )
-            }
-            </ul>;
+
+  // TODO: figure out what does this react hooks do
+  const [products, setProduct] = useState([]);
+
+  // TODO: figure out what this useEffect do 
+  useEffect(() =>{
+    const fetchData = async () => {
+      const {data} = await axios.get("/api/products");
+      setProduct(data);
+    }
+
+    fetchData();
+
+    return () => {
+      //
+    };
+  }, [])
+
+  return <ul className="products">
+    {
+      products.map(product => 
+        <li key={product._id}>
+          <div className="product">
+            <Link to={'/product/' + product._id}>
+                <img
+                  className="product-image"
+                  src={product.image}
+                  alt="product"
+                />
+            </Link>
+            <div className="product-name">
+              <Link to={'/product/' + product._id}>{product.name}</Link>
+            </div>
+            <div className="product-brand">{product.brand}</div>
+            <div className="product-price">${product.price}</div>
+            <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
+          </div>
+        </li>
+      )
+    }
+    </ul>;
 }
 
 export default HomeScreen;
