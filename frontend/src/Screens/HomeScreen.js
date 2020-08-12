2471a5e49c7ae9bd53666d53d9242acc0a93af29
Zhongjie Shen <zhongjis@uci.edu>
2020-08-11 21:01:29 -0700

    [feature] add redux for product list

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,49 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { listProducts } from "../actions/productAction";
 
 function HomeScreen(props) {
-
   // TODO: figure out what does this react hooks do
-  const [products, setProduct] = useState([]);
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
 
-  // TODO: figure out what this useEffect do 
-  useEffect(() =>{
-    const fetchData = async () => {
-      const {data} = await axios.get("/api/products");
-      setProduct(data);
-    }
-
-    fetchData();
+  // TODO: figure out what this useEffect do
+  useEffect(() => {
+    dispatch(listProducts());
 
     return () => {
       //
     };
-  }, [])
+  }, []);
 
-  return <ul className="products">
-    {
-      products.map(product => 
+  return loading ? (
+    <div>Loading ... </div>
+  ) : error ? (
+    <div> {error} </div>
+  ) : (
+    <ul className="products">
+      {products.map((product) => (
         <li key={product._id}>
           <div className="product">
-            <Link to={'/product/' + product._id}>
-                <img
-                  className="product-image"
-                  src={product.image}
-                  alt="product"
-                />
+            <Link to={"/product/" + product._id}>
+              <img
+                className="product-image"
+                src={product.image}
+                alt="product"
+              />
             </Link>
             <div className="product-name">
-              <Link to={'/product/' + product._id}>{product.name}</Link>
+              <Link to={"/product/" + product._id}>{product.name}</Link>
             </div>
             <div className="product-brand">{product.brand}</div>
             <div className="product-price">${product.price}</div>
-            <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
+            <div className="product-rating">
+              {product.rating} Stars ({product.numReviews} Reviews)
+            </div>
           </div>
         </li>
-      )
-    }
-    </ul>;
+      ))}
+    </ul>
+  );
 }
 
 export default HomeScreen;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+        <App />
+    </Provider>
+  ,
   document.getElementById('root')
 );
 
