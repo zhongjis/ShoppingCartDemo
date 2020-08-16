fe9c50b6395945d4949ebd679bfc061e4fbc9e19
Zhongjie Shen <zhongjis@uci.edu>
2020-08-15 23:26:14 -0700

    [backend] add /api/products/:id to backend

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,17 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
+app.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(x => x._id === productId);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({msg: "Product Not Found."});
+    }
+    res.send();
+});
+
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
 });
