16be9fa329f3491946c5ec66713dec2874fbbc16
Zhongjie Shen <zhongjis@uci.edu>
2020-08-16 12:23:18 -0700

    [CI] update server.js with deployment info for frontend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 import data from './data';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/api/products", (req, res) => {
     res.send(data.products);
@@ -19,6 +21,10 @@ app.get("/api/products/:id", (req, res) => {
     res.send();
 });
 
-app.listen(5000, () => {
-    console.log('Server started at http://localhost:5000');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/../frontend/build')));
+}
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost: ${PORT} `);
 });
