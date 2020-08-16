472a54ab7f9046cbdefc35b4ede7b0cc2e5b4086
Zhongjie Shen <zhongjis@uci.edu>
2020-08-16 00:05:49 -0700

    [feature] generate product quantity based on countInStock

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { detailsProduct } from '../actions/productAction';
+import { detailsProduct } from "../actions/productAction";
 
 function ProductScreen(props) {
-    const productDetails = useSelector(state => state.productDetails);
+    const [qty, setQty] = useState(1);
+    const productDetails = useSelector((state) => state.productDetails);
     const { product, loading, error } = productDetails;
     const dispatch = useDispatch();
 
@@ -53,11 +54,17 @@ function ProductScreen(props) {
                             <li>Status: {product.status}</li>
                             <li>
                                 Qty:{" "}
-                                <select>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
+                                <select
+                                    value={qty}
+                                    onChange={(e) => {
+                                        setQty(e.target.value);
+                                    }}
+                                >
+                                    {[
+                                        ...Array(product.countInStock).keys(),
+                                    ].map((x) => (
+                                        <option value={x + 1}> {x + 1} </option>
+                                    ))}
                                 </select>
                             </li>
                             <li>
